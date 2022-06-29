295584ee60fb812bf46610283e052de9a08bb55b
phanuwit13 <big.phanuwit@gmail.com>
2022-06-30 02:17:39 +0700

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,24 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [state, setstate] = useState()
-  return <div className='App'>My Birth Day</div>
+  const [time, setTime] = useState(new Date())
+
+  setInterval(() => {
+    let current = new Date()
+    setTime(current)
+  }, 1000) //1000 millisecond = 1 second
+
+  return (
+    <div className='App'>
+      Current Time
+      <div>
+        {time.getDate()} - {time.getMonth() + 1} - {time.getFullYear()}
+      </div>
+      <div>
+        {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
+      </div>
+    </div>
+  )
 }
 
 export default App
