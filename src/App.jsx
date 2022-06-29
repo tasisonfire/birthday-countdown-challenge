d8a632871eed3423a9feebf6f22365a3076d7a8e
phanuwit13 <big.phanuwit@gmail.com>
2022-06-30 02:06:45 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [state, setstate] = useState()
+  return <div className='App'>My Birth Day</div>
+}
+
+export default App
