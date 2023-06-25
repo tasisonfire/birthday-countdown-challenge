cf6237cb8b98887579e3e261615f9e16e73e9d87
tasisonfire <tasudomsiri@gmail.com>
2023-06-26 00:32:35 +0700

    looking around

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   //state
-  const [time, setTime] = useState(new Date())
+  const [time, setTime] = useState(new Date());
+
+  const timeNow = new Date();
+  const getMonth = timeNow.getMonth() + 1;
+  const dayNow = Math.floor(timeNow / (60 * 60 * 24));
 
   //function input TimeStamp and then return day,hours,minutes and seconds
   const getCountdown = (birthDate) => {
-    let now = new Date().getTime()
-    let timeCount = birthDate - now //convert milliseconds to seconds
+    let now = new Date().getTime();
+    let timeCount = birthDate - now; //convert milliseconds to seconds
 
-    let days = Math.floor(timeCount / (60 * 60 * 24)) //convert seconds to days
+    let days = Math.floor(timeCount / (60 * 60 * 24)); //convert seconds to days
     let hours = Math.floor(
       (timeCount % (60 * 60 * 24)) / (60 * 60) //convert seconds to hours
-    )
+    );
     // let minutes
     // let seconds
 
-    return [days, hours]
-  }
+    return [days, hours];
+  };
 
   useEffect(() => {
     const intervalTask = setInterval(() => {
-      setTime(new Date())
-    }, 1000) //1000 millisecond = 1 second
-    return () => clearInterval(intervalTask)
-  }, [])
+      setTime(new Date());
+    }, 1000); //1000 millisecond = 1 second
+    return () => clearInterval(intervalTask);
+  }, []);
 
   return (
-    <div className='container'>
-      {time.getDate()}/{time.getMonth() + 1}/{time.getFullYear()}{' '}
+    <div className="container">
+      {getMonth}
+      <br />
+      {time.getDate()}/{time.getMonth() + 1}/{time.getFullYear()}{" "}
       {time.getHours()}:{time.getMinutes() + 1}:{time.getSeconds()}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
